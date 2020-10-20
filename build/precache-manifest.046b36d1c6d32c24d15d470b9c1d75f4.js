390d3ee475c0f053f2877e2db1b4bb3f165d1ccd
Carl Padilla <carlpadilla@protonmail.com>
2020-10-20 10:56:37 -0400

    deployed to netlify

---
+++ build/precache-manifest.046b36d1c6d32c24d15d470b9c1d75f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa265458310d107948edb6c2ae2234e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5bda53affb4d9563e0c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "3583298480aa9e42d07e",
+    "url": "/static/css/main.ebf57e39.chunk.css"
+  },
+  {
+    "revision": "b5bda53affb4d9563e0c",
+    "url": "/static/js/2.4ea6cd3b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4ea6cd3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3583298480aa9e42d07e",
+    "url": "/static/js/main.9f796828.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "e6e10fdf66cf9f4f84d2b269586cf040",
+    "url": "/static/media/batmfo__.e6e10fdf.woff"
+  }
+]);
